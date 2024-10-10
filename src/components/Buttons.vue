cd730364c1494a0eeea166ce02062f5a5e2ac9a9
Avalsida <vladic.er.g@gmail.com>
2024-10-10 17:14:19 +0200

    Commit

---
+++ src/components/Buttons.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button>{{ label }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            label: String,
+            color: String
+
+        }
+    }
+</script>
+
+<style scoped>
+    button{
+        background-color: v-bind(color);
+        padding: 7px 20px;
+}
