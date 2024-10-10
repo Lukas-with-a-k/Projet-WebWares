039fd7644bf37868fd1132c09c6abfbed0298c2c
«Lukas»git <lukas.pti@live.fr>
2024-10-10 15:38:51 +0200

    head & foot components

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <H1>Landing Page</H1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped> 
+.background-color{  background-color: #3F4666;
+}
+</style>
