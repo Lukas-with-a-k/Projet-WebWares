f79853d014afa41b744c5b4f8b95114c7351d952
«Lukas»git <lukas.pti@live.fr>
2024-10-15 10:06:19 +0200

    merge claudia

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,15 +1,15 @@
 <template>
     <div>
-        <ButtonComponent/>
+        
     </div>
 </template>
 
 <script>
 
-import ButtonComponent from '/components/ButtonComponent.vue';
+
 export default {
     components: {
-        ButtonComponent
+       
     },
 }
 </script>
