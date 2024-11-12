467adc5c7106ed8da0b5e085db4228be879a7767
«Lukas»git <lukas.pti@live.fr>
2024-11-12 15:57:31 +0100

    iii

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/https://lukas-with-a-k.github.io/Projet-WebWares//'
+    : '/'
+}
